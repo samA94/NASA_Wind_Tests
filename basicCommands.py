9c41e31fd9ad12855a5f87da35989233d226b816
samA94 <samallison94@gmail.com>
2017-01-12 13:58:58 -0600

    Add files via upload

--- basicCommands.py
+++ basicCommands.py
@@ -5,38 +5,51 @@ from mavros_msgs.msg import GlobalPositionTarget
 import time
 
 def set_Waypoint(read_Position, travel_Height, travel_Direction, travel_Distance, velocity):
+    waypoint = GlobalPositionTarget()
+    waypoint.coordinate_frame = GlobalPositionTarget.FRAME_GLOBAL_INT
 
-    if target_Distance < 0.002:
+    travel_Direction = int(travel_Direction)
+
+    waypoint.header.stamp = rospy.get_rostime()
+    waypoint.header.frame_id = "1"
+    waypoint.type_mask = 0b0001111111111111
+
+
+    if travel_Distance < 0.002:
 
         waypoint.velocity.x = velocity[0]
         waypoint.velocity.y = velocity[1]
         waypoint.velocity.z = velocity[2]
 
-        if target_Direction == 0:
+        if travel_Direction == 0:
             waypoint.latitude = read_Position.latitude + 0.001 + travel_Distance
             waypoint.longitude = read_Position.longitude
             waypoint.altitude = travel_Height
+            waypoint.yaw = 3.14/2
 
-        elif target_Direction == 1:
+        elif travel_Direction == 1:
             waypoint.latitude = read_Position.latitude - 0.001 - travel_Distance
             waypoint.longitude = read_Position.longitude
             waypoint.altitude = travel_Height
+            waypoint.yaw = 3.14*3/2
 
-        elif target_Direction == 2:
+        elif travel_Direction == 2:
             waypoint.longitude = read_Position.longitude + 0.001 + travel_Distance
             waypoint.latitude = read_Position.latitude
             waypoint.altitude = travel_Height
+            waypoint.yaw = 0
 
-        elif target_Direction == 3:
-            waypoint.longitude = read_Position.longitude + 0.001 + travel_Distance
+        elif travel_Direction == 3:
+            waypoint.longitude = read_Position.longitude - 0.001 - travel_Distance
             waypoint.latitude = read_Position.latitude
             waypoint.altitude = travel_Height
+            waypoint.yaw = 3.14
 
         else:
             print "Error"
             waypoint.velocity.x = 0
             waypoint.velocity.y = 0
-            wayoint.velocity.z = 0
+            waypoint.velocity.z = 0
 
         return waypoint
 
@@ -46,63 +59,19 @@ def set_Waypoint(read_Position, travel_Height, travel_Direction, travel_Distance
         while True:
             1==1
 
-def has_Reached_Position(Target_Position, read_Position):
+def has_Reached_Position(final_Target_Position, read_Position):
     #function to check if the quad has reached the target position
 
     lat = read_Position.latitude
     lon = read_Position.longitude
     alt = read_Position.altitude
 
-    distance_to_Lat = lat - final_Target_Position[0]
-    distance_to_Lon = lon - final_Target_Position[1]
-    distance_to_alt = alt - final_Target_Position[2]
+    distance_to_Lat = lat - final_Target_Position.latitude
+    distance_to_Lon = lon - final_Target_Position.longitude
 
-    if alt > 0.95 * final_Target_Position[2] and distance_to_Lat < 0.0001 and distance_to_Lon < 0.0001:
+    if alt > 0.95 * final_Target_Position.altitude and distance_to_Lat < 0.0001 and distance_to_Lon < 0.0001:
         return True
     else:
         return False
-
-
-def quad_Command(mode, armVar):
-    #initialize topics for arming quad
-    rospy.wait_for_service("mavros/cmd/arming")
-    armQuad = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
-    rospy.wait_for_service("mavros/set_mode")
-    modeSet = rospy.ServiceProxy("mavros/set_mode", SetMode) 
-
-    #arm quadrotor and initialize proper mode
-
-    armQuad(armVar)
-    print "System Arm Status: ", armVar
-    time.sleep(3)
-
-    modeSet(mode[0], mode[1])
-    print "Mode set to: ", mode
-
-
-
-def takeoff(height, pub_Position, read_Position):
-    #quadrotor takeoff to desired height
-
-    initial_height_Target = GlobalPositionTarget()
-
-    current_Lat = read_Position.latitude
-    current_Lon = read_Position.longitude
-
-    if read_Position.altitude < 0.95 * height:
-        initial_height_Target.altitude = height
-        initial_height_Target.latitude = current_Lat
-        initial_height_Target.longitude = current_Lon
-        initial_height_Target.velocity.x = 0
-        initial_height_Target.velocity.y = 0
-        initial_height_Target.velocity.z = 1
-
-        print "The current height is: " + read_Position.altitude
-        pub_Position.publish(initial_height_Target)
-
-        return False
-
-    elif read_Position.altitude >= 0.95 * height:
-        return True
 
 
--- px4_ROS_Fly.py
+++ px4_ROS_Fly.py
@@ -17,35 +17,25 @@ rospy.Rate(50.0)
 
 global read_Position
 
+filename = input("Please enter the desired file name, with the extension.")
+
+pos_File = open(filename, 'w')
 
 def position_callback(GPS_Position_From_Quad):
     #function to get the position of the quad
     global read_Position
     read_Position = GPS_Position_From_Quad
 
+    with open(filename, 'w') as pos_File:
+        pos_File.write(read_Position.latitude + ',')
+        pos_File.write(read_Position.longitude + ',')
+        pos_File.write(read_Position.altitude + '\n')
 
 def main():
     global read_Position
 
-
-    #Take target location inputs
-    target_Direction = input("Please enter whether you would rather initially travel
-            north=0, south=1, east=2, or west=3, as an integer")
-
-    target_Distance = input("Enter the distance you with to travel in this direction, in degrees")
-    target_Height = input("Enter the height you would like to travel at, in meters")
-
-    turn_Direction = input("Enter the direction you would like to turn in, using the above format")
-    turn_Distance = input("Enter the distance you would like to travel in this direction, in degrees")
-
-    travel_Velocity = input("Enter the desired travel velocity in m/s, in [forward, left, up] coordinates")
-
-
     rospy.Subscriber("/mavros/global_position/global", PoseWithCovariance,
         position_callback)
-
-    mode_List = [0, "OFFBOARD"]
-    quad_Command(mode_List, True)
 
     #Set home position
     home_Position = read_Position
@@ -53,26 +43,57 @@ def main():
     #Assign tuple with maximum allowable altitude
     max_Height = (read_Position.altitude + 50,)
     ground_Level = (read_Position.altitude,)
-    
-    pub_Position = rospy.Publisher("/mavros/setpoint_raw/local", PositionTarget, queue_size = 10)
+    print "The home position is: ", home_Position
+
+
+    #Take target location inputs
+    target_Direction = input("Please enter whether you would rather initially travel
+            north=0, south=1, east=2, or west=3, as an integer")
+
+    target_Distance = input("Enter the distance you with to travel in this direction, in degrees")
+    travel_Height = input("Enter the height you would like to travel at, in meters")
+
+    turn_Direction = input("Enter the direction you would like to turn in, using the above format")
+    turn_Travel_Time = input("Enter the time you would like to travel in this direction, in seconds")
+
+    initial_Travel_Velocity = input("Enter the desired travel velocity in m/s, in
+            [North_vel, East_vel, Down_vel, yaw_rate] coordinates")
+
+    after_Turn_Travel_Velocity = input("Enter the desired velocity after the turn, using the same
+            coordinate convention.")
+
+    #Set the goal location for the turn
+    final_Target_Position = waypoint(home_Position, travel_Height+ground_Level, 
+            target_Direction, travel_Distance-.001, travel_Velocity)
+
+    print "The position for the turn location has been set as: ", final_Target_Position
+
+    pub_Position = rospy.Publisher("/mavros/setpoint_raw/global", PositionTarget, queue_size = 10)
     
     #initialize waypoints
     waypoint1 = PositionTarget()
     waypoint2 = PositionTarget()
 
+    dummyVar = input("Press enter after the quad has been armed, set to OFFBOARD mode, and has taken off.")
+
+    takeoff_Waypoint = waypoint(home_Position, travel_Height+ground_Level, target_Direction,
+                -0.001, [0,0,0,travel_velocity[3]])
+
     #take off to requested height
-    if target_Height+ground_Level < max_Height[0]:
-        check = False
-        while check==False:
-            check = takeoff(target_Height+ground_Level, pub_Position, read_Position)
-            time.sleep(0.2)
+    while read_Position.altitude - ground_Level < 0.95 * travel_Height:
+        pub_Position.publish(takeoff_Waypoint)
+        time.sleep(0.4)
+        print "Taking off.  The height is: " read_Position.altitude - ground_Level       
+
+    print "The desired height has been reached: ", read_Position.altitude - ground_Level
 
     #set the first waypoint location and velocity
-    waypoint1 = set_Waypoint(read_Position, travel_Height+ground_Level, travel_Direction,
+    waypoint1 = set_Waypoint(home_Position, travel_Height+ground_Level, travel_Direction,
             travel_Distance, travel_Velocity)
 
     #publish waypoint
     pub_Position.publish(waypoint1)
+    print "The first waypoint is set at: ", waypoint1
 
     while True:
         #resend the waypoint and check to see if the quad has
@@ -84,33 +105,47 @@ def main():
 
         else:
             pub_Position.publish(waypoint1)
-            time.sleep(0.1)
+            print "The current location is: ", read_Position
+            time.sleep(0.4)
 
-    #Set turn waypoint
+
     waypoint2 = set_Waypoint(read_Position, travel_Height+ground_Level, turn_Direction,
-            turn_Distance, travel_Velocity)
+            .001, after_Turn_Travel_Velocity)
 
     start_Time = time.time()
-
-    while start_Time - time.time() < 5:
-        pub.publish(waypoint2)
-        time.sleep(0.1)
-
-    #publish home waypoint
-    home_Waypoint.latitude = home_Position.latitude
-    home_Waypoint.longitude = home_Position.longitude
-    home_Waypoint.altitude = travel_Height+ground_Level
+    #Published the turn
+    while time.time() - start_Time < turn_Travel_Time:
+        pub_Position.publish(waypoint2)
+        print "The current location is: ", read_Position
+        time.sleep(0.4)
     
 
+    print "The destination has been reached.  Please use the return to launch feature on 
+           the controller or on your ground control station"
 
-    #land
+    end_Waypoint = waypoint(read_Position, travel_Height+ground_Level, turn_Direction,
+                -0.001, [0,0,0,travel_velocity[3]])
+    pub_Position.publish(end_Waypoint)
+
+if __name__ == '__main__':
+    main()
 
 
 
-    #disarm
+#Possible changes and improvements:
 
+#All sections need to be tested
 
+#Assuming NED conventions.  However, this is not explicitly stated in documentation
 
+#Yaw is not explained in documentation.  Currently assuming radians, but might need to be changed
+#Also, no explanation of yaw mechanics is given - does it stop and turn, or turn while flying?
 
+#Velocities have similar issues.  Assumed to be global, but they could be local
+
+#Could automate arming, mode set, and takeoff, but would not be as safe or controllable
+#Might be useful to have ROS automatically send quad home, but don't currently see a RTL function in mavros
+
+#Information was gathered from mavros msg documentation, ROS standards, and mavlink msg documentation
 
 
